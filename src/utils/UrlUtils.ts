f0095607a8d3a19bec4acd78860912f70154b390
Samuel Benkovič <samuel.benkovic@thermofisher.com>
2023-09-20 12:34:55 +0200

    fix parse url

--- src/utils/UrlUtils.ts
+++ src/utils/UrlUtils.ts
@@ -8,8 +8,16 @@ export class UrlUtils {
     public static readParams(url: string, responseMode: "query" | "fragment" = "query"): URLSearchParams {
         if (!url) throw new TypeError("Invalid URL");
         // the base URL is irrelevant, it's just here to support relative url arguments
-        const parsedUrl = new URL(url, "http://127.0.0.1");
-        const params = parsedUrl[responseMode === "fragment" ? "hash" : "search"];
+        var parsedUrl = new URL(url, "http://127.0.0.1");
+
+        // for the case we have url like  http://127.0.0.1/myApp#/path/to/authcallback?code=myCode&state=myState
+        if(responseMode === "fragment"){
+            var paramsHash = parsedUrl["hash"].slice(1);
+            console.log(paramsHash)
+            parsedUrl = new URL(paramsHash, "http://127.0.0.1");
+        }
+
+        const params = parsedUrl["search"];
         return new URLSearchParams(params.slice(1));
     }
 }
